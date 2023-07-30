eef0f327ed3ed5eff8af74a3765ef4bad5b24c8a
madu <maduamaral2006@gmail.com>
2023-07-30 16:37:45 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Cidades do Litoral Sul de São Paulo:");
+Console.WriteLine("Bertioga");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Santos");
+Console.WriteLine("São Vicente");
+Console.WriteLine("Praia Grande");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Itanhaém");
+Console.WriteLine("Peruíbe");
+Console.WriteLine("Iguape");
+Console.WriteLine("Ilha Comprida");
+Console.WriteLine("Cananéia");
